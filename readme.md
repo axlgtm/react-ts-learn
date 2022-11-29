13cc6b178015bd092df457a1dd1b6f3f872fa988
axlgtm <fancycyanogen@gmail.com>
2022-11-29 10:21:18 +0800

    checkpoint

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Learn how derived state and memoization works
+## video source : https://www.youtube.com/watch?v=E1cklb4aeXA&t=260s
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState<string[]>([])
 
-  return (
-    <div className="App">
-      hola
-    </div>
-  )
+  function onSubmit (e: FormEvent<HTMLFormElement>) {
+
+  }
+
+  return (<>
+    <label>
+      Search :
+      <input type="search" />
+    </label>
+    <br />
+    <br />
+    <form onSubmit={onSubmit}>
+      <label>
+        New Item: 
+        <input type="text" />
+      </label>
+      <button type='submit'>Add</button>
+    </form>
+    <h1>Items: </h1>
+    {
+      items.map((item, index) => <p key={index}>{item}</p>)
+    }
+  </>)
 }
 
 export default App
